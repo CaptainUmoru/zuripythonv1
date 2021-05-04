90d3c2d05a5fbe785a84e693388c0331bc852962
CaptainUmoru <52971417+CaptainUmoru@users.noreply.github.com>
2021-05-05 00:46:40 +0100

    Update Budget Class App.py

--- Budget Class App.py
+++ Budget Class App.py
@@ -1,4 +1,4 @@
-class Budget:
+class leanBudget:
     
     def __init__(self, category, amount):
         self.category = category
@@ -19,10 +19,10 @@ class Budget:
 
 
 
-category = Budget("Unto God", 50000)
-category_1 = Budget("Clothes", 5000)
-category_2 = Budget("Internet", 30,000)
-category_3 = Budget("Transportation", 15000)
-category_4 = Budget("Food", 25000)
+category = leanBudget("Unto God", 50000)
+category_1 = leanBudget("Clothes", 5000)
+category_2 = leanBudget("Internet", 30,000)
+category_3 = leanBudget("Transportation", 15000)
+category_4 = leanBudget("Food", 25000)
 
 print(category.deposit())
